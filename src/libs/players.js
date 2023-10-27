a31ed6369ea0e75f3350a52fbfc27af580cc1dd7
Nasimur Rahman Rhythm <nasimurrahman4538@gmail.com>
2023-10-27 13:06:54 +0600

    20% done

--- src/libs/players.js
+++ src/libs/players.js
@@ -1,8 +1,8 @@
-export default teams = {
+export default players = {
     Bangladesh : ["Tanzid Hasan","Litton Das","Mehidy Hasan Miraz","Najmul Hossain Shanto","Shakib Al Hasan (c)","Mushfiqur Rahim (wk)","Towhid Hridoy","Mahmudullah","Taskin Ahmed","Shoriful Islam","Mustafizur Rahman"],
     SriLanka: ["Pathum Nissanka", "Kusal Perera", "Kusal Mendis (c & wk)", "Sadeera Samarawickrama", "Charith Asalanka", "Dhananjaya de Silva", "Angelo Mathews", "Maheesh Theekshana", "Kasun Rajitha", "Lahiru Kumara", "Dilshan Madushanka"],
     England : ["Jonny Bairstow", "Dawid Malan", "Joe Root", "Harry Brook", "Moeen Ali", "Jos Buttler (wk/c)", "Liam Livingstone", "Sam Curran", "Chris Woakes", "Adil Rashid", "Mark Wood"],
-    NewZeLands: ["Devon Conway", "Will Young", "Rachin Ravindra", "Daryl Mitchell", "Tom Latham (c & wk)", "Glenn Phillips", "Mark Chapman", "Mitchell Santner", "Matt Henry", "Lockie Ferguson", "Trent Boult"],
+    NewZeLand: ["Devon Conway", "Will Young", "Rachin Ravindra", "Daryl Mitchell", "Tom Latham (c & wk)", "Glenn Phillips", "Mark Chapman", "Mitchell Santner", "Matt Henry", "Lockie Ferguson", "Trent Boult"],
     Australia : ["David Warner", "Mitchell Marsh", "Steven Smith", "Marnus Labuschagne", "Glenn Maxwell", "Alex Carey (wk)", "Cameron Green", "Pat Cummins (c)", "Mitchell Starc", "Adam Zampa", "Josh Hazlewood"],
     India : ["Rohit Sharma (c)", "Ishan Kishan", "Virat Kohli", "Shreyas Iyer", "KL Rahul (wk)", "Hardik Pandya", "Ravindra Jadeja", "Ravichandran Ashwin", "Kuldeep Yadav", "Jasprit Bumrah", "Mohammed Siraj"],
     Netherlands: ["Vikramjit Singh", "Max ODowd", "Colin Ackermann", "Bas de Leede", "Teja Nidamanuru", "Scott Edwards (c & wk)", "Sybrand Engelbrecht", "Logan van Beek", "Roelof van der Merwe", "Aryan Dutt", "Paul van Meekeren"],
@@ -10,6 +10,8 @@ export default teams = {
     SouthAfrica: ["Quinton de Kock (wk)", "Temba Bavuma (c)", "Rassie van der Dusen", "Aiden Markram", "Heinrich Klaasen", "David Miller", "Marco Jansen", "Kagiso Rabada", "Keshav Maharaj", "Lungi Ngidi", "Gerald Coetzee"],
     Afghanistan : ["Rahmanullah Gurbaz (wk)", "Ibrahim Zadran", "Rahmat Shah", "Hashmatullah Shahidi (c)", "Mohammad Nabi", "Najibullah Zadran", "Azmatullah Omarzai", "Rashid Khan", "Mujeeb Ur Rahman", "Naveen-ul-Haq", "Fazalhaq Farooqi"]
 };
+
+
 
 
 
---
+++ src/libs/teams.js
@@ -0,0 +1,2 @@
+const teams = ["Bangladesh", "SriLanka", "England", "NewZeLand", "Australia", "India", "Netherlands", "Pakistan", "SouthAfrica", "Afghanistan"];
+export default teams;
